46993871de40088206317f20a97b1af0617e5912
shengjie55555 <shengjie55555@gmail.com>
2022-11-03 19:36:50 +0800

    feat(model): add atds & more loss layer

--- model/loss.py
+++ model/loss.py
@@ -3,12 +3,12 @@ import torch.nn as nn
 from utils.data_utils import gpu
 
 
-class Loss(nn.Module):
+class VectorNetLoss(nn.Module):
     def __init__(self, cfg, device):
-        super(Loss, self).__init__()
+        super(VectorNetLoss, self).__init__()
         self.cfg = cfg
         self.device = device
-        self.pred_loss = PredLoss(cfg)
+        self.pred_loss = VectorNetPredLoss(cfg)
 
     def forward(self, out, data):
         loss_out = self.pred_loss(out, gpu(data["trajs_fut"], self.device), gpu(data["pad_fut"], self.device))
@@ -17,9 +17,24 @@ class Loss(nn.Module):
         return loss_out
 
 
-class PredLoss(nn.Module):
+class ATDSLoss(nn.Module):
+    def __init__(self, config, device):
+        super(ATDSLoss, self).__init__()
+        self.config = config
+        self.device = device
+        self.pred_loss = ATDSPredLoss(config)
+
+    def forward(self, out, data):
+        loss_out = self.pred_loss(out, gpu(data["gt_preds"], self.device), gpu(data["has_preds"], self.device))
+        loss_out["loss"] = loss_out["cls_loss"] / (loss_out["num_cls"] + 1e-10) + \
+                           loss_out["reg_loss"] / (loss_out["num_reg"] + 1e-10) + \
+                           loss_out["key_points_loss"] / (loss_out["num_key_points"] + 1e-10)
+        return loss_out
+
+
+class VectorNetPredLoss(nn.Module):
     def __init__(self, cfg):
-        super(PredLoss, self).__init__()
+        super(VectorNetPredLoss, self).__init__()
         self.cfg = cfg
         self.reg_loss = nn.SmoothL1Loss(reduction="sum")
 
@@ -81,5 +96,87 @@ class PredLoss(nn.Module):
             reg[has_preds], gt_preds[has_preds]
         )
         loss_out["num_reg"] += has_preds.sum().item()
+
+        return loss_out
+
+
+class ATDSPredLoss(nn.Module):
+    def __init__(self, config):
+        super(ATDSPredLoss, self).__init__()
+        self.config = config
+        self.reg_loss = nn.SmoothL1Loss(reduction="sum")
+
+    def forward(self, out, gt_preds, has_preds):
+        cls, reg, key_points = out["cls"], out["reg"], out["key_points"]
+        cls = torch.cat([x for x in cls], 0)
+        reg = torch.cat([x for x in reg], 0)
+        key_points = torch.cat([x for x in key_points], 0)
+        gt_preds = torch.cat([x for x in gt_preds], 0)
+        has_preds = torch.cat([x for x in has_preds], 0)
+
+        loss_out = dict()
+        zero = 0.0 * (cls.sum() + reg.sum())
+        loss_out["cls_loss"] = zero.clone()
+        loss_out["num_cls"] = 0
+        loss_out["reg_loss"] = zero.clone()
+        loss_out["num_reg"] = 0
+        loss_out["key_points_loss"] = zero.clone()
+        loss_out["num_key_points"] = 0
+
+        num_mods, num_preds = self.config["num_mods"], self.config["num_preds"]
+
+        last = has_preds.float() + 0.1 * torch.arange(num_preds).float().to(has_preds.device) / float(num_preds)
+        max_last, last_idcs = last.max(1)
+        mask = max_last > 1.0
+
+        cls = cls[mask]
+        reg = reg[mask]
+        key_points = key_points[mask]
+        gt_preds = gt_preds[mask]
+        has_preds = has_preds[mask]
+        last_idcs = last_idcs[mask]
+
+        row_idcs = torch.arange(len(last_idcs)).long().to(last_idcs.device)
+        dist = []
+        for j in range(num_mods):
+            dist.append(
+                torch.sqrt(
+                    (
+                            (reg[row_idcs, j, last_idcs] - gt_preds[row_idcs, last_idcs])
+                            ** 2
+                    ).sum(1)
+                )
+            )
+        dist = torch.cat([x.unsqueeze(1) for x in dist], 1)
+        min_dist, min_idcs = dist.min(1)
+        row_idcs = torch.arange(len(min_idcs)).long().to(min_idcs.device)
+
+        mgn = cls[row_idcs, min_idcs].unsqueeze(1) - cls
+        mask0 = (min_dist < self.config["cls_th"]).view(-1, 1)
+        mask1 = dist - min_dist.view(-1, 1) > self.config["cls_ignore"]
+        mgn = mgn[mask0 * mask1]
+        mask = mgn < self.config["mgn"]
+        coef = self.config["cls_coef"]
+        loss_out["cls_loss"] += coef * (
+                self.config["mgn"] * mask.sum() - mgn[mask].sum()
+        )
+        loss_out["num_cls"] += mask.sum().item()
+
+        reg = reg[row_idcs, min_idcs]
+        coef = self.config["reg_coef"]
+        loss_out["reg_loss"] += coef * self.reg_loss(
+            reg[has_preds], gt_preds[has_preds]
+        )
+        loss_out["num_reg"] += has_preds.sum().item()
+
+        key_points_idx = self.config["key_points"]
+        key_points = key_points[row_idcs, min_idcs]
+        has_preds = has_preds[:, [-1, key_points_idx[0], key_points_idx[1], -1]]
+        gt_preds = gt_preds[:, [-1, key_points_idx[0], key_points_idx[1], -1]]
+        coef = self.config["key_points_coef"]
+        loss_out["key_points_loss"] += coef * self.reg_loss(
+            key_points[has_preds], gt_preds[has_preds]
+        )
+        loss_out["num_key_points"] = has_preds.sum().item()
 
         return loss_out
---
+++ utils/log_utils.py
@@ -0,0 +1,175 @@
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from argoverse.evaluation.eval_forecasting import get_displacement_errors_and_miss_rate
+
+
+class Logger(object):
+    def __init__(self, enable=True, log_dir=""):
+        self.enable = enable
+        self.log_dir = log_dir
+
+        if self.enable:
+            self.writer = SummaryWriter(self.log_dir)
+        print("logger enable_flags: {}, log_dir: {}".format(self.enable, self.log_dir))
+
+    def add_scalar(self, title, value, it):
+        if self.enable:
+            if isinstance(value, torch.Tensor):
+                value = value.detach().cpu().item()
+            self.writer.add_scalar(title, value, it)
+
+    def add_dict(self, data, it, prefix=""):
+        if self.enable:
+            for key, val in data.items():
+                title = prefix + key
+                self.add_scalar(title, val, it)
+
+
+class VectorNetAverageLoss(object):
+    def __init__(self):
+        self.loss_out = {}
+
+    def reset(self):
+        self.loss_out = {}
+
+    def update(self, loss_out):
+        # initialization
+        if len(self.loss_out.keys()) == 0:
+            for key in loss_out.keys():
+                if key != "loss":
+                    self.loss_out[key] = 0
+
+        for key in loss_out.keys():
+            if key == "loss":
+                continue
+            if isinstance(loss_out[key], torch.Tensor):
+                self.loss_out[key] += loss_out[key].detach().cpu().item()
+            else:
+                self.loss_out[key] += loss_out[key]
+
+    def get(self):
+        cls = self.loss_out["cls_loss"] / (self.loss_out["num_cls"] + 1e-10)
+        reg = self.loss_out["reg_loss"] / (self.loss_out["num_reg"] + 1e-10)
+        loss = cls + reg
+        loss_out = {
+            "cls_loss": cls,
+            "reg_loss": reg,
+            "loss": loss
+        }
+        return loss_out
+
+
+class VectorNetAverageMetrics(object):
+    def __init__(self, cfg):
+        self.reg = {}
+        self.cls = {}
+        self.gts = {}
+        self.city = {}
+        self.cfg = cfg
+
+    def reset(self):
+        self.reg, self.cls, self.city, self.gts = {}, {}, {}, {}
+
+    def update(self, post_out, data):
+        reg = [x[0:1].detach().cpu().numpy() for x in post_out["reg"]]
+        cls = [x[0:1].detach().cpu().numpy() for x in post_out["cls"]]
+        for j, seq_id in enumerate(data["seq_id"]):
+            self.reg[seq_id] = reg[j].squeeze()
+            self.cls[seq_id] = [x for x in cls[j].squeeze()]
+            self.gts[seq_id] = data["trajs_fut"][j][0].detach().cpu().numpy()
+            self.city[seq_id] = data["city_name"][j]
+
+    def get(self):
+        res_1 = get_displacement_errors_and_miss_rate(
+            self.reg, self.gts, 1, self.cfg["pred_len"], 2, self.cls)
+        res_k = get_displacement_errors_and_miss_rate(
+            self.reg, self.gts, self.cfg["num_mode"], self.cfg["pred_len"], 2, self.cls)
+
+        metrics_out = {
+            'minade_1': res_1['minADE'],
+            'minfde_1': res_1['minFDE'],
+            'mr_1': res_1['MR'],
+            'brier_fde_1': res_1['brier-minFDE'],
+            'minade_k': res_k['minADE'],
+            'minfde_k': res_k['minFDE'],
+            'mr_k': res_k['MR'],
+            'brier_fde_k': res_k['brier-minFDE']
+        }
+
+        return metrics_out
+
+
+class ATDSAverageLoss(object):
+    def __init__(self):
+        self.loss_out = {}
+
+    def reset(self):
+        self.loss_out = {}
+
+    def update(self, loss_out):
+        # initialization
+        if len(self.loss_out.keys()) == 0:
+            for key in loss_out.keys():
+                if key != "loss":
+                    self.loss_out[key] = 0
+
+        for key in loss_out.keys():
+            if key == "loss":
+                continue
+            if isinstance(loss_out[key], torch.Tensor):
+                self.loss_out[key] += loss_out[key].detach().cpu().item()
+            else:
+                self.loss_out[key] += loss_out[key]
+
+    def get(self):
+        cls = self.loss_out["cls_loss"] / (self.loss_out["num_cls"] + 1e-10)
+        reg = self.loss_out["reg_loss"] / (self.loss_out["num_reg"] + 1e-10)
+        key_points = self.loss_out["key_points_loss"] / (self.loss_out["num_key_points"] + 1e-10)
+        loss = cls + reg + key_points
+        loss_out = {
+            "cls_loss": cls,
+            "reg_loss": reg,
+            "key_points": key_points,
+            "loss": loss
+        }
+        return loss_out
+
+
+class ATDSAverageMetrics(object):
+    def __init__(self, cfg):
+        self.reg = {}
+        self.cls = {}
+        self.gts = {}
+        self.city = {}
+        self.cfg = cfg
+
+    def reset(self):
+        self.reg, self.cls, self.city, self.gts = {}, {}, {}, {}
+
+    def update(self, post_out, data):
+        reg = [x[0:1].detach().cpu().numpy() for x in post_out["reg"]]
+        cls = [x[0:1].detach().cpu().numpy() for x in post_out["cls"]]
+        for j, seq_id in enumerate(data["argo_id"]):
+            self.reg[seq_id] = reg[j].squeeze()
+            self.cls[seq_id] = [x for x in cls[j].squeeze()]
+            self.gts[seq_id] = data["gt_preds"][j][0].detach().cpu().numpy()
+            self.city[seq_id] = data["city"][j]
+
+    def get(self):
+        res_1 = get_displacement_errors_and_miss_rate(
+            self.reg, self.gts, 1, self.cfg["num_preds"], 2, self.cls)
+        res_k = get_displacement_errors_and_miss_rate(
+            self.reg, self.gts, self.cfg["num_mods"], self.cfg["num_preds"], 2, self.cls)
+
+        metrics_out = {
+            'minade_1': res_1['minADE'],
+            'minfde_1': res_1['minFDE'],
+            'mr_1': res_1['MR'],
+            'brier_fde_1': res_1['brier-minFDE'],
+            'minade_k': res_k['minADE'],
+            'minfde_k': res_k['minFDE'],
+            'mr_k': res_k['MR'],
+            'brier_fde_k': res_k['brier-minFDE']
+        }
+
+        return metrics_out
